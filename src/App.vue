0516feefcd3df7b041c3718e14b2f7ba309ca6ef
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-14 18:17:17 +0200

    feat: add to app.vue 3 keys (url construction)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <!--todo 1d1fd003d3798bdc399e491c87adb22f api_key -->
 <script>
+/* COMPONENTS import  */
 import AppHeader from "./components/AppHeader.vue";
-// import MyComponent from "./components/MyComponent.vue";
+
+//AXIOS
+import axios from "axios";
 
 export default {
   data() {
     return {
+      movieAPI: "https://api.themoviedb.org/3/search/movie?query=",
+      tvSeriesAPI: "https://api.themoviedb.org/3/search/tv?query=",
+      api_key: "&api_key=1d1fd003d3798bdc399e491c87adb22f",
       title: "Hello world",
     };
   },
-
+  //Components
   components: {
     AppHeader,
+  },
+
+  //Methods
+  methods: {
+    getMovie() {
+      axios.get(movieAPI).then((response) => {});
+    },
   },
 };
 </script>
