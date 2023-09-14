e2bb08a0b8dd20858fab383839ca9a46c40bed15
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-14 16:33:14 +0200

    feat: create Project folder and APP

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+/* Bootstrap */
+import "bootstrap/scss/bootstrap";
+import * as bootstrap from "bootstrap";
+
+/* font awesome */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+createApp(App).mount("#app");
