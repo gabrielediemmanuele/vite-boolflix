f3620c85682eed841e74f8752cce95cec869f2f8
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-18 18:26:24 +0200

    fix: clear app.vue from  trash comments

--- src/App.vue
+++ src/App.vue
@@ -26,18 +26,12 @@ export default {
     AppHeader,
     AppMain,
   },
-  //Methods
+
   methods: {
-    //NOTA - query viene tramandato tramite emit da AppHeader.
-    //nome del metodo (per i film)
+    // metodo (per i film)
     getMovie(query) {
-      //creo una variabile che racchiuda l'url dei film
       let movieUrl = `${this.movieApi}${query}${this.api_key}`;
-      //con axios col get prendo la variabile e col response la risposta dell'url
       axios.get(movieUrl).then((response) => {
-        //asseggno all'array filteredTvSeries dichiarato in data, il risultato
-        // mi avvalgo di usare .map per creare un nuovo "array" cono solo i contenuti che mi
-        //servono e posso anche aggiungerene, in caso di bisogno.
         this.store.filteredMovies = response.data.results.map((movie) => {
           const {
             id,
@@ -54,18 +48,15 @@ export default {
             language: original_language,
             vote: Math.ceil(vote_average / 2),
             moviesPoster: poster_path,
-            //vote: Math.ceil(vote_average / 2),
           };
         });
         console.log(this.store.filteredMovies);
       });
     },
-    //per le serie tv
+    // metodo per le serie tv
     getTvSeries(query) {
-      //variabile serie tv
       let tvSeries = `${this.tvSeriesApi}${query}${this.api_key}`;
       console.log(tvSeries);
-      //con axios col get prendo la variabile e col response la risposta dell'url
       axios.get(tvSeries).then((response) => {
         this.store.filteredTvSeries = response.data.results.map((tvSerie) => {
           const {
@@ -83,7 +74,6 @@ export default {
             language: original_language,
             vote: Math.ceil(vote_average / 2),
             tvSeriesPoster: poster_path,
-            //vote: Math.ceil(vote_average / 2),
           };
         });
         console.log(this.store.filteredTvSeries);
@@ -94,17 +84,12 @@ export default {
       this.getTvSeries(query);
     },
   },
-  // in realtà, la chiamata axios va _creata_
-  //ma invece  creiamo un metodo versatile e su created richiamiamo
-  //il nome del metodo cosi che venga "creato".
+
   created() {},
 };
 </script>
 
 <template>
-  <!--* AppHeader si collega al suo Emit per dire nei methods a 
-  getMovie di ricevere sia l'evento di ricerca
-  che il contenuto "query"  -->
   <AppHeader @re-search="getLists"></AppHeader>
   <AppMain> </AppMain>
 </template>
